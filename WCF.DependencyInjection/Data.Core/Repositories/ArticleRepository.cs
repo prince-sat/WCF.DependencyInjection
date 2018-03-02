9b5e12c7e0fed5f56ba57661d4107fcd9719698f
EXT_HBE57 <EXT_HBE57@PAR1DSK0319>
2018-03-02 17:06:10 +0100

    implementation de la bibliotheque de classe service

---
+++ WCF.DependencyInjection/Data.Core/Repositories/ArticleRepository.cs
@@ -0,0 +1,28 @@
+﻿using Business.Entities;
+using Data.Core.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Core.Repositories
+{
+    public class ArticleRepository : RepositoryBase<Article>, IArticleRepository
+    {
+        public ArticleRepository(IDbFactory dbFactory)
+            : base(dbFactory) { }
+
+        public Article GetArticleByTitle(string articleTitle)
+        {
+            var _article = this.DbContext.ArticleSet.Where(b => b.Title == articleTitle).FirstOrDefault();
+
+            return _article;
+        }
+    }
+
+    public interface IArticleRepository : IRepository<Article>
+    {
+        Article GetArticleByTitle(string articleTitle);
+    }
+}
