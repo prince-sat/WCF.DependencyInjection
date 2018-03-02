525b2e32f711a7e7288235356159cc637816cbba
EXT_HBE57 <EXT_HBE57@PAR1DSK0319>
2018-03-02 17:35:33 +0100

    appliquer les meme namespaces pout les entites business et entites clients

--- WCF.DependencyInjection/Client.Entities/Properties/AssemblyInfo.cs
+++ WCF.DependencyInjection/Client.Entities/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
 
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ContractNamespace("http://www.chsakell.com/WCF_DI",
+                              ClrNamespace = "Client.Entities")]
