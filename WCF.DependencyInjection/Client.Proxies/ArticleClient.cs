5ca455e02ea15033cc604aeb7f65c1ce5670fcba
EXT_HBE57 <EXT_HBE57@PAR1DSK0319>
2018-03-05 16:43:21 +0100

    implementation de projet proxies test

--- WCF.DependencyInjection/Client.Proxies/ArticleClient.cs
+++ WCF.DependencyInjection/Client.Proxies/ArticleClient.cs
@@ -6,48 +6,50 @@ using System.Linq;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
-
-public class ArticleClient : ClientBase<IArticleService>, IArticleService
+namespace Client.Proxies
 {
-    #region IArticleService Members
-    public void Add(Article article)
+    public class ArticleClient : ClientBase<IArticleService>, IArticleService
     {
-        Channel.Add(article);
-    }
-
-    public void Update(Article article)
-    {
-        Channel.Update(article);
-    }
-
-    public void Delete(Article article)
-    {
-        Channel.Delete(article);
-    }
-
-    public Article GetById(int id)
-    {
-        return Channel.GetById(id);
-    }
-
-    public Article[] GetAll()
-    {
-        return Channel.GetAll();
-    }
-    #endregion
-
-    public void CleanUp()
-    {
-        try
+        #region IArticleService Members
+        public void Add(Article article)
         {
-            if (base.State != CommunicationState.Faulted)
-                base.Close();
-            else
-                base.Abort();
+            Channel.Add(article);
         }
-        catch (Exception ex)
+
+        public void Update(Article article)
         {
-            base.Abort();
+            Channel.Update(article);
+        }
+
+        public void Delete(Article article)
+        {
+            Channel.Delete(article);
+        }
+
+        public Article GetById(int id)
+        {
+            return Channel.GetById(id);
+        }
+
+        public Article[] GetAll()
+        {
+            return Channel.GetAll();
+        }
+        #endregion
+
+        public void CleanUp()
+        {
+            try
+            {
+                if (base.State != CommunicationState.Faulted)
+                    base.Close();
+                else
+                    base.Abort();
+            }
+            catch (Exception ex)
+            {
+                base.Abort();
+            }
         }
     }
 }
---
+++ WCF.DependencyInjection/Test.Proxies/ClientInjectionClass.cs
@@ -0,0 +1,53 @@
+﻿using Autofac.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Proxies
+{
+    public class ClientInjectionClass : Data.Core.Infrastructure.Disposable
+    {
+        public Client.Contracts.IBlogService _blogProxy;
+        public Client.Contracts.IArticleService _articleProxy;
+        public ClientInjectionClass(Client.Contracts.IArticleService articleServiceProxy,
+            Client.Contracts.IBlogService blogServiceProxy)
+        {
+            this._blogProxy = blogServiceProxy;
+            this._articleProxy = articleServiceProxy;
+        }
+
+        #region IDisposable
+        protected override void DisposeCore()
+        {
+            base.DisposeCore();
+            try
+            {
+                (_blogProxy as Client.Proxies.BlogClient).CleanUp();
+
+                (_articleProxy as Client.Proxies.ArticleClient).CleanUp();
+            }
+            catch
+            {
+                _blogProxy = null;
+                _articleProxy = null;
+            }
+        }
+        #endregion
+
+        #region Methods
+
+        public Client.Entities.Article[] GetArticles()
+        {
+            return _articleProxy.GetAll();
+        }
+
+        public Client.Entities.Blog GetBlogById(int id)
+        {
+            return _blogProxy.GetById(id);
+        }
+
+        #endregion
+    }
+}
